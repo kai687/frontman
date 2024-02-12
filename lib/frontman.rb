e7c43c2518fd54925fd5828a94293ad33dd89966
Kai Welke <kai.welke@algolia.com>
2024-02-12 19:41:44 +0100

    fix: sort imports

--- lib/frontman.rb
+++ lib/frontman.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'frontman/errors'
 require 'frontman/app'
 require 'frontman/context'
 require 'frontman/config'
 require 'frontman/custom_struct'
 require 'frontman/data_store'
 require 'frontman/data_store_file'
-require 'frontman/sitemap_tree'
+require 'frontman/errors'
 require 'frontman/resource'
+require 'frontman/sitemap_tree'
