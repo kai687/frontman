a1019ba73fd375b1525745a2cee7d22c1f72653b
Kai Welke <kai.welke@algolia.com>
2024-02-22 13:57:41 +0100

    fix: haml works but prints double

--- docs/src/helpers/render_helper.rb
+++ docs/src/helpers/render_helper.rb
@@ -6,15 +6,7 @@ module RenderHelper
     partial_dir = Frontman::Config.get(:partial_dir)
     partial  = File.join(partial_dir, template)
     resource = Frontman::Resource.from_path(partial, nil, false)
-    app = Frontman::App.instance
 
-    current_page = app.current_page
-    app.current_page = resource if current_page.nil?
-
-    content = resource.render(block.call, data)
-
-    app.current_page = current_page
-
-    content
+    resource.render((block.call if block_given?), data)
   end
 end
