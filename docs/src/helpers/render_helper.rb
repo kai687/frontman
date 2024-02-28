34085af3a73bbb9d9d6d39461ccae65fd9040fa5
Kai Welke <kai.welke@algolia.com>
2024-02-28 16:51:00 +0100

    fix: try template suggestions

--- docs/src/helpers/render_helper.rb
+++ docs/src/helpers/render_helper.rb
@@ -7,8 +7,17 @@ module RenderHelper
   def partial_block(template, data = {}, &block)
     partial_dir = Frontman::Config.get(:partial_dir)
     partial = File.join(partial_dir, template)
+    resource = Frontman::Resource.from_path(partial, nil, false)
     context = Frontman::Context.new
     content = context.get_content_buffer(nil, &block)
-    Frontman::Resource.from_path(partial, nil, false).render(content, data)
+
+    current_page = Frontman::App.instance.current_page
+    Frontman::App.instance.current_page = resource if current_page.nil?
+
+    content = resource.render(content, data)
+
+    Frontman::App.instance.current_page = current_page
+
+    content
   end
 end
