8a16ad96e59b230609f0fa179cda96cb3b81e537
Kai Welke <kai.welke@algolia.com>
2024-02-28 09:40:12 +0100

    fix: try get_content_buffer

--- docs/src/helpers/render_helper.rb
+++ docs/src/helpers/render_helper.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require 'frontman/context'
+
 # Renders a partial with block
 module RenderHelper
   def partial_block(template, data = {}, &block)
     partial_dir = Frontman::Config.get(:partial_dir)
-    partial  = File.join(partial_dir, template)
-    resource = Frontman::Resource.from_path(partial, nil, false)
-
-    resource.render((block.call if block_given?), data)
+    partial = File.join(partial_dir, template)
+    context = Frontman::Context.new
+    content = context.get_content_buffer(nil, &block)
+    Frontman::Resource.from_path(partial, nil, false).render(content, data)
   end
 end
